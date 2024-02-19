8ed3ef9e49eee32edbf657706ec198c811514469
Semih AÇMALI <suhedasemihacmali@gmail.com>
2024-02-19 14:43:37 +0300

    Delete src directory

--- src/Hafta3/Odev.java
+++
@@ -1,8 +0,0 @@
-package Hafta3;
-
-public class Odev {
-
-    //MBP104-Ileri-Programlama-02-Temel-Sozdizimi-Degiskenler-ve-Turler dosyasındaki
-    // Sayfa 24, 25, 26, 27 sorumlu değilsiniz.
-    //Sayfa 40'daki soruyu cozun.
-}
